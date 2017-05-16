d67e493e3e141d9937f8992f11949c7350372962
zedhicks <gottrash@icloud.com>
2017-05-16 13:27:02 -0400

    Initial Commit

---
+++ mytodoMacApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mytodoMacApp
+//
+//  Created by Emmanuel Vachon on 2017-05-16.
+//  Copyright © 2017 artdram. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
