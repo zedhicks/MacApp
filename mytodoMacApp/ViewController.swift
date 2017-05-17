1beb9678dc2266ef16e869be2ad68d55daf4ed53
zedhicks <gottrash@icloud.com>
2017-05-16 21:37:10 -0400

    21h30 first try at a MacApp

--- mytodoMacApp/ViewController.swift
+++ mytodoMacApp/ViewController.swift
@@ -10,18 +10,53 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var inputField: NSTextField!
+    
+    @IBOutlet weak var importantCheckBox: NSButton!
+    
+    var toDos : [Todo] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    
+  /*  func getTodo() {
+        // get coredata
+        if let context = (NSApplication.shared().delegate as? AppDelegate)?.persistentContainer.viewContext {
+            do {
+                toDos = try context.fetch(Todo.fetchRequest())
+        } catch{}
+        
+        //set property
+        
+        //display on table
+        
+        }*/
+    
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    @IBAction func addClick(_ sender: Any) {
+        if inputField.stringValue != "" {
+            if let context = (NSApplication.shared().delegate as? AppDelegate)?.persistentContainer.viewContext {
+                let toDo = Todo(context: context)
+                toDo.name = inputField.stringValue
+                if importantCheckBox.state == 0 {
+                    toDo.important = false }
+                else {
+                    toDo.important = true}
+              (NSApplication.shared().delegate as? AppDelegate)?.saveAction(nil)
+                inputField.stringValue = ""
+                importantCheckBox.state = 0
+               // getTodo()
+                }
+                    
+                }
+                
+        
         }
+        
+        
     }
-
-
-}
-
